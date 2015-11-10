8d99058261fff21c669068df1ac776c88ef53e0d
vokoshyv <ronsuij@gmail.com>
2015-11-10 13:32:19 -0800

    (feat) adds consolidate requirement in server.js file for engine designation

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
+// Node requirements
 var app = require('express')();
 var server = require('http').Server(app);
+
+// Handlebar requirement
+var engines = require('consolidate');
 
 app.get('/wechat', function(req, res){
   res.send('<h1>Hello world</h1>');
 });
+
+
 
 module.exports = server;
