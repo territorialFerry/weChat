7b21e6b0a0cf07c43d28ab01060d594a20a8379f
vokoshyv <ronsuij@gmail.com>
2015-11-10 14:06:28 -0800

    (feat) adds routes in server.js file

--- server/server.js
+++ server/server.js
@@ -12,8 +12,25 @@ app.set('view engine', 'hbs');
 
 
 
-app.get('/wechat', function(req, res){
+app.get('/wechat', function(req, res, next){
   res.send('<h1>Hello world</h1>');
+});
+
+app.get('/wechat/signin', function(req, res, next){
+  res.send('<h1>Hello signin</h1>');
+});
+
+app.get('/wechat/signup', function(req, res, next){
+  res.send('<h1>Hello signup</h1>');
+});
+
+app.get('/wechat/rooms', function(req, res, next){
+  res.send('<h1>Hello rooms</h1>');
+});
+
+app.get('/wechat/chat/:chat', function(req, res, next){
+  var room = req.params.chat
+  res.send('<h1>Hello ' + room + '</h1>');
 });
 
 
