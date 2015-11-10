a976d7327306bbe4698fb0173600eed2fee29e11
vokoshyv <ronsuij@gmail.com>
2015-11-10 13:47:33 -0800

    (feat) adds Handlebars view engine

--- server/server.js
+++ server/server.js
@@ -5,6 +5,13 @@ var server = require('http').Server(app);
 // Handlebar requirement
 var engines = require('consolidate');
 
+// Application View Engine Setup
+app.engine('hbs', engines.handlebars);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
+
+
+
 app.get('/wechat', function(req, res){
   res.send('<h1>Hello world</h1>');
 });
