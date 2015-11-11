7ebc1b2d42d4c02c50abe9df592d63e8bf168951
vokoshyv <ronsuij@gmail.com>
2015-11-11 14:26:00 -0800

    (feat) adds POST routes to userRoutes.js

--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -2,6 +2,7 @@ var userController = require('./userController.js');
 
 module.exports = function(app){
   app.post('/signin', userController.checkAuth);
-  app.get('/signin', userController.signin);
-  app.get('/signup', userController.signup);
+  app.post('/signup', userController.signup);
+  app.get('/signinPage', userController.signinPage);
+  app.get('/signupPage', userController.signupPage);
 }
