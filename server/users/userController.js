7eef1074972f94e5a1ac66f4df4cb797da39fe99
vokoshyv <ronsuij@gmail.com>
2015-11-10 18:37:47 -0800

    (feat) adds plans and handlebars views in userController.js

--- server/users/userController.js
+++ server/users/userController.js
@@ -1,10 +1,16 @@
 module.exports = {
   signin: function(req, res, next){
-    res.send('bwahahah');
+    // logic for signing in
+    // allow user to proceed to room page
+    // or let them know that signing in didn't work
+    res.render('signin');
   }, 
 
   signup: function(req, res, next){
-
+    // logic for signing up
+    // allow user to proceed to room page
+    // or let them know that username was not valid
+    res.render('signup');
   }, 
 
   checkAuth: function(req, res, next){
