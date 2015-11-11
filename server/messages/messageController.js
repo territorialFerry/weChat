6318c14d205b7048a107b5dfcc4ed55058c7bc33
vokoshyv <ronsuij@gmail.com>
2015-11-10 17:08:33 -0800

    (feat) initializes file for messageController.js

---
+++ server/messages/messageController.js
@@ -0,0 +1,13 @@
+module.exports = {
+  rooms: function(req, res, next){
+    res.send('take blah');
+  }, 
+
+  chat: function(req, res, next){
+
+  }, 
+
+  private: function(req, res, next){
+
+  }
+}
