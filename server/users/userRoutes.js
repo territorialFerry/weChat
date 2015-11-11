060e81df297f4f7bd26e6902ab369c41ec1bcd50
vokoshyv <ronsuij@gmail.com>
2015-11-11 12:56:49 -0800

    (feat) adds POST route for checking authenticity of user

--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -1,6 +1,7 @@
 var userController = require('./userController.js');
 
 module.exports = function(app){
+  app.post('/signin', userController.checkAuth);
   app.get('/signin', userController.signin);
   app.get('/signup', userController.signup);
 }
