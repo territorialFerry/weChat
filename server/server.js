c532219dbd4d46383bfb9d0279ebc6ae5eeb7384
vokoshyv <ronsuij@gmail.com>
2015-11-10 17:03:44 -0800

    (feat) adds middleware requirement in server.js file

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 // Node requirements
-var app = require('express')();
+var express = require('express');
+var app = express();
 var server = require('http').Server(app);
 
 // Handlebar requirement
@@ -10,27 +11,8 @@ app.engine('hbs', engines.handlebars);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
 
-// Routes
-app.get('/wechat', function(req, res, next){
-  res.send('<h1>Hello world</h1>');
-});
-
-app.get('/wechat/signin', function(req, res, next){
-  res.send('<h1>Hello signin</h1>');
-});
-
-app.get('/wechat/signup', function(req, res, next){
-  res.send('<h1>Hello signup</h1>');
-});
-
-app.get('/wechat/rooms', function(req, res, next){
-  res.send('<h1>Hello rooms</h1>');
-});
-
-app.get('/wechat/chat/:chat', function(req, res, next){
-  var room = req.params.chat
-  res.send('<h1>Hello ' + room + '</h1>');
-});
+// Middleware requirement
+require('./config/middleware.js')(app, express);
 
 // Export the server
 module.exports = server;
