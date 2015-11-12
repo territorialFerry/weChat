11413cf503dc454b1b3a94e0ce88aae6e671ab6e
vokoshyv <ronsuij@gmail.com>
2015-11-11 21:03:37 -0800

    (feat) adds logic to userController.js for signing in to wechat

--- server/users/userController.js
+++ server/users/userController.js
@@ -5,51 +5,87 @@ var secret = 'interwebs';
 
 module.exports = {
   signinPage: function(req, res, next){
-    // logic for signing in
-    // allow user to proceed to room page
-    // or let them know that signing in didn't work
-    res.render('signin');
+    // landing page
+    res.render('landing');
   }, 
 
   signupPage: function(req, res, next){
-    // logic for signing up
-    // allow user to proceed to room page
-    // or let them know that username was not valid
+    // signup page
     res.render('signup');
   }, 
 
   checkAuth: function(req, res, next){
     // checking to see if users are allowed on a certain page
+    var payload = req.body;
+    var username = req.body.username;
+    var userPW = req.body.password; 
 
+    // check if user exists
+    usersDB.query("select * from userpassword where username = '" + username + "'", function(err, rows, fields){
+      if (err){console.log(err)};
 
+      // user does not exist in table
+      if (rows.length === 0){
+        res.render('landing', {
+          'userNotValid': 'Sorry, that username does not exist',
+          'username': username
+        });
+        return;
+      } else {
+        var databasePWHash = rows[0].password;
 
-    console.log(req.body);
-    console.log(req.connection.remoteAddress);
-    res.render('rooms')
+        // verify user-inputted password matches corresponding
+        // password from database
+        password(userPW).verifyAgainst(databasePWHash, function(error, verified){
+          if(error)
+              throw new Error('Something went wrong!');
+          if(!verified) {
+              console.log("Hacker!! We got you!");
+              res.render('landing', {
+                'username': username, 
+                'passwordNotValid': 'Sorry, that password is not valid'
+              })
+              return;
+          } else {
+              console.log("You're in!!");
+              var token = jwt.encode(payload, secret);
+              res.cookie('wechatToken', token);
+              res.render('rooms');
+              return;
+          }
+        })
+      }
+    })
   }, 
 
   signup: function(req, res, next){
     // signing up a new user
+    var payload = req.body;
     var username = req.body.username;
     var userPW = req.body.password;
-
-    console.log("USERNAME: ", username);
-    console.log("userPW: ", userPW);
-    console.log("COOKIES: ", req.cookies);
     
+    // verification of username and password length
     if (username.length < 8){
       res.render('signup', {
         'usernameLength': 'Sorry, username must be at least 8 characters long', 
         'username': username
       });
+      return;
+    } else if (username.length > 30){
+      res.render('signup', {
+        'usernameLength': 'Sorry, username must be less than 30 characters long', 
+        'username': username
+      });
+      return;
     } else if (userPW.length < 8){
       res.render('signup', {
         'passwordLength': 'Sorry, password must be at least 8 characters long', 
         'username': username
       });
+      return;
     }
 
-    // query usersDB is username is already taken
+    // query usersDB if username is already taken
     usersDB.query("select * from userpassword where username = '" + username + "'", function(err, rows, fields){
       if (err){console.log(err)}
 
@@ -58,6 +94,7 @@ module.exports = {
           'username': username, 
           'taken': 'Sorry, that username is already taken'
         });
+        return;
       } else {
         // hash password, insert into mysql database, set up
         // cookieand render rooms page
@@ -66,13 +103,14 @@ module.exports = {
 
           usersDB.query("insert into userpassword (username, password) values ('" + username + "', '" + hashedPW + "');", function(err, rows, fields){
             if (err){throw err}
-            res.cookie('wechatToken', 1234);
+
+            var token = jwt.encode(payload, secret);
+
+            res.cookie('wechatToken', token);
             res.render('rooms');
           })
         })
       }
     });
-
-
   }
 }
