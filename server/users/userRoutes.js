d143669db19253147bc46ace1710a928d6bca579
vokoshyv <ronsuij@gmail.com>
2015-11-11 21:05:15 -0800

    (feat) adds GET route for signinPage

--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -4,6 +4,7 @@ module.exports = function(app){
   app.post('/signin', userController.checkAuth);
   app.post('/signup', userController.signup);
   app.get('/signup', userController.signupPage);
-  app.get('/signinPage', userController.signinPage);
+  app.get('/signin', userController.signinPage);
+  // app.get('/signinPage', userController.signinPage);
   // app.get('/signupPage', userController.signupPage);
 }
