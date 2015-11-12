26ae63ee335686a0dd5b364eea16c35f039d4dbc
vokoshyv <ronsuij@gmail.com>
2015-11-11 18:44:37 -0800

    (feat) creates get route for signup route

--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -3,6 +3,7 @@ var userController = require('./userController.js');
 module.exports = function(app){
   app.post('/signin', userController.checkAuth);
   app.post('/signup', userController.signup);
+  app.get('/signup', userController.signupPage);
   app.get('/signinPage', userController.signinPage);
-  app.get('/signupPage', userController.signupPage);
+  // app.get('/signupPage', userController.signupPage);
 }
