4a44e0deb071b3caf93ccbc6e64dd081919e3f0a
vokoshyv <ronsuij@gmail.com>
2015-11-11 18:42:48 -0800

    (feat) adds logic for signing up a new user

--- server/users/userController.js
+++ server/users/userController.js
@@ -1,3 +1,7 @@
+var usersDB = require('./userModel.js');
+var password = require('password-hash-and-salt');
+var secret = 'interwebs';
+
 module.exports = {
   signinPage: function(req, res, next){
     // logic for signing in
@@ -16,6 +20,8 @@ module.exports = {
   checkAuth: function(req, res, next){
     // checking to see if users are allowed on a certain page
 
+
+
     console.log(req.body);
     console.log(req.connection.remoteAddress);
     res.render('rooms')
@@ -23,7 +29,48 @@ module.exports = {
 
   signup: function(req, res, next){
     // signing up a new user
+    var username = req.body.username;
+    var userPW = req.body.password;
 
-    console.log('blah');
+    console.log("USERNAME: ", username);
+    console.log("userPW: ", userPW);
+    console.log("COOKIES: ", req.cookies);
+    
+    if (username.length < 8){
+      res.render('signup', {
+        'usernameLength': 'Sorry, username must be at least 8 characters long', 
+        'username': username
+      });
+    } else if (userPW.length < 8){
+      res.render('signup', {
+        'passwordLength': 'Sorry, password must be at least 8 characters long', 
+        'username': username
+      });
+    }
+
+    // query usersDB is username is already taken
+    usersDB.query("select * from userpassword where username = '" + username + "'", function(err, rows, fields){
+      if (err){console.log(err)}
+
+      if (rows.length > 0){
+        res.render('signup', {
+          'username': username, 
+          'taken': 'Sorry, that username is already taken'
+        });
+      } else {
+        // hash password, insert into mysql database, and render rooms page
+        password(userPW).hash(function(error, hashedPW){
+          if (error){throw error}
+
+          usersDB.query("insert into userpassword (username, password) values ('" + username + "', '" + hashedPW + "');", function(err, rows, fields){
+            if (err){throw err}
+            res.cookie('signedIn', 1234);
+            res.render('rooms');
+          })
+        })
+      }
+    });
+
+
   }
 }
