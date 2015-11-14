ef6dfc9da799267c4d41725cd6b74ad4f2dd26a5
vokoshyv <ronsuij@gmail.com>
2015-11-13 19:40:21 -0800

    (feat) adds functions in utils.js file for retrieval of messages information

--- server/config/utils.js
+++ server/config/utils.js
@@ -1,12 +1,50 @@
 var messagesDB = require('../messages/messageModel.js');
+var io = require('../socketConnection.js');
 
 module.exports = {
-  grabRoomData: function(req, res, next){
+  grabRoomsData: function(req, res, next){
     messagesDB.query('select * from messages;', function(err, rows, fields){
       if (err){console.log(error)};
 
-      console.log("ROWS: ", rows);
-      console.log("FIELDS: ", fields);
+      var roomRef = {};
+      
+      rows.forEach(function(row){
+        if (roomRef[row.room] === undefined){
+          roomRef[row.room] = {};
+          roomRef[row.room][row.username] = true;
+        } else {
+          roomRef[row.room][row.username] = true;
+        }
+      })
+
+      var roomRefKeys = Object.keys(roomRef);
+      var usersPerRoom = {};
+
+      roomRefKeys.forEach(function(key){
+        usersPerRoom[key] = Object.keys(roomRef[key]).length;
+      })
+
+      console.log("USERSPERROOM: ", usersPerRoom);
+
+      res.render('rooms', {'usersPerRoom': usersPerRoom});
+      return;
+
     })
+  }, 
+
+  retrieveMessages: function(req, res, next, room){
+    messagesDB.query("select * from messages where room = '" + room + "';", function(err, rows, fields){
+      if (err){console.log(error)};
+
+      // console.log("ROWS: ", rows);
+      // var socket = io();
+      
+      res.render('chat', {'messages': rows});
+      return;
+    })
+  }, 
+
+  addMessage: function(req, res, next){
+
   }
 }
